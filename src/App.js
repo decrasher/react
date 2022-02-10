bef421349dce4aebf95e67ed760bf979004fe2c3
decrasher <audrlek007@gmail.com>
2022-02-10 15:51:20 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,36 @@ import Subject from './components/Subject';
 
 class App extends Component {
   constructor(props){
+    super(props);
+    this.state = {
+      mode:'welcome',
+      subject:{title:'Web',sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, react'},
+      contents:[
+        {id:1, title:'HTML',desc:'HTML is HyperText....'},
+        {id:2, title:'CSS',desc:'CSS is for design'},
+        {id:3, title:'JavaScript',desc:'JavaScript is for interactive'}
+      ]
+    }
     
   }
   render(){
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode ==='read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-         <Subject title="react" sub="hi"></Subject>
+         <Subject 
+            title={this.state.subject.title} 
+            sub={this.state.subject.sub}></Subject>
          <Subject title="react" sub="world wide web!"></Subject>
-         <TOC></TOC>
-         <Content title="HTML" desc="HTML is HyperText markup Language."></Content>
+         <TOC data={this.state.contents}></TOC>
+         <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
